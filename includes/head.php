236e875478653e87bfc26fb8fa7f3966b6eeb1cc
Maddox37 <pratasfpopras@gmail.com>
2025-07-24 13:26:31 -0500

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -3,4 +3,4 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    <!-- Cartagena ponga aqui los CSS que valla a usar  -->
-    <link href="/resources/scripts/stylisher.css" rel="stylesheet">
+    <link href="https://htmlpreview.github.io/?https://github.com/Maddox37/website/blob/main/resources/scripts/stylisher.css" rel="stylesheet">
