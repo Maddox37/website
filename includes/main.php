9b5703a2d265d45ee0157917d44ca48de3c16831
Maddox37 <pratasfpopras@gmail.com>
2025-08-12 14:22:26 -0500

    Update main.php

--- includes/main.php
+++ includes/main.php
@@ -1,9 +1,1 @@
-<div id="topbar"> <!-- Añada secciones aqui si quiere.  -->
-       <a href="/index.php"><i class="fa fa-wpforms" style="font-size:24px;margin-right:10px"></i>INVENTORY</a>
-        <nav class="topbar-nav">
-            <a style="margin-left: 490px;" href="/sections/home.php">Inicio</a>
-            <a href="#">Nosotros</a>
-             <a id="sessionLink" href="/sections/login.php">Iniciar Sesión</a>
-        </nav>
-    </div>
-    <div style="margin-top: 50px;"></div>
+<!--No dispobile en la version beta-->
